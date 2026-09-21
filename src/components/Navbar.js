602edab9987a685b782b310aff5300b40916d45e
Arnav Bule <arnav.bule05@gmail.com>
2026-09-21 18:46:24 +0530

    feat: new AB logo in white, retire arnav.fun
    Navbar brand uses the new AB mark in solid white (sized proportionally) for contrast on the dark background. Removes arnav.fun references: package homepage, footer credit, and the Blogs nav item (blogs.arnav.fun no longer resolves). The fork/star button links this repo, and the contribution calendar shows GODOSTROYER instead of the template author.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
+import logo from "../Assets/logo.svg";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
@@ -39,7 +38,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <img src={logo} className="img-fluid logo" alt="Arnav Bule logo" />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -92,20 +91,11 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="https://blogs.arnav.fun"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/GODOSTROYER"
+                href="https://github.com/GODOSTROYER/My-Portfolio-Website"
                 target="_blank"
+                rel="noreferrer"
                 className="fork-btn-inner"
               >
                 <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
